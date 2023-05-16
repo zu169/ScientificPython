e1ae95d863ec540b995e0b9642ea7effb33e66bb
zuz2 <zuz2@aber.ac.uk>
2023-05-16 17:57:35 +0100

    add new worksheet files

--- 3-DescriptiveStats/worksheet3.py
+++ 3-DescriptiveStats/worksheet3.py
@@ -1,6 +1,9 @@
 import numpy as np
-import scipy as sp
+import pandas as pd
+import scipy as sp 
 from scipy import stats
+import matplotlib.pyplot as plt
+
 
 def question1():
     print("Question 1: \n")
@@ -15,8 +18,80 @@ def question1():
     
     skewness = stats.skew(uniform_array)
     kurtosis = stats.kurtosis(uniform_array)
-    print("\n\nSkewness: ", skewness, "\nKurtosis: ", kurtosis)
+    #Skewness - measure of symmetry 
+    #Kurtosis - measure of the tailedness of a distribution (how often outliers occur)
+    print("\nSkewness: ", skewness, "\nKurtosis: ", kurtosis)
     
+def question2():
+    print("\nQuestion 2: ")
+    normal_array  = np.random.normal(loc=50, scale=10, size=100) 
+    print("\nNormally-Distributed Array:", normal_array)
+    
+    mean = np.mean(normal_array)
+    median = np.median(normal_array)
+    variance = np.var(normal_array)
+    std_dev = np.std(normal_array)
+    print("\nMean: ", mean, "\nMedian: ", median, "\nVariance: ", variance, "\nStandard Deviation: ", std_dev)
+    
+    skewness = stats.skew(normal_array)
+    kurtosis = stats.kurtosis(normal_array)
+    print("\nSkewness: ", skewness, "\nKurtosis: ", kurtosis)
+    
+def question3():
+    print("\nQuestion 3:")
+    df = pd.read_csv('camden_trees.csv')
+    #describe function prints count,mean,std(min,25%,50%,75,max) for each column in dataframe
+    summary_stats = df.describe()
+    print("\nSummary Statistics for Camden Trees: \n", summary_stats)
+    
+    #plot Height column as a histogram
+    df['Height'].hist()
+    plt.xlabel('Height (m)')
+    plt.ylabel('Frequency')
+    plt.title('Height Distribution of Camden Trees')
+    plt.show()
+    
+    plt.savefig('height_histogram.png')
+    plt.clf()
+    
+    #plot Spread column as a historgram
+    df['Spread'].hist()
+    plt.title('Spread Distribution of Camden Trees')
+    plt.xlabel('Spread (m)')
+    plt.ylabel('Frequency')
+    plt.show()
+    
+    plt.savefig('spread_histogram.png')
 
+    #Group the data by tree maturity
+    grouped_data = df.groupby('Maturity')
+    
+    #Plot the grouped Height column as histograms
+    histograms = []
+    for name,group in grouped_data:
+        histogram = group['Height'].hist(alpha=0.5, label=name)
+        histograms.append(histogram)
+        
+    plt.title('Height Distribution of Camden Trees by Maturity')
+    plt.xlabel('Height (m)')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.show()
+    
+    plt.savefig('height_maturity_histogram.png')
+    
+    
+    #Plot max Spread columns
+    max_spread = grouped_data['Spread'].max()
+    ax = max_spread.plot.bar()
+    ax.set_title('Maximum Spread for each age of tree')
+    ax.set_xlabel('Maturity')
+    ax.set_ylabel('Maximum Spread (m)')
+    plt.tight_layout()
+    plt.show()
+    
+    plt.savefig('max_spread_maturity_histogram')
     
 question1()
+question2()
+question3()
---
+++ 4-ConfidenceIntervals/worksheet4.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy import stats
+
+def population_mean_test(population, population_mean):
+    #sample 100 individuals randomly
+    sample = np.random.choice(population, size=100, replace=False)
+    #calculate mean and standard deviation
+    sample_mean = np.mean(sample)
+    sample_std = np.std(sample, ddof=1)
+    
+    #print("Sample mean: ", sample_mean)
+    #print("Sample standard deviation: ", sample_std)
+    
+    #calculate an estimate of the standard error of the mean by 
+    #dividing the sample standard deviation by square root of sample size
+    
+    #Standard error of the mean (SEM) - measures the variability of sample means
+    sem = sample_std / np.sqrt(len(sample))
+    
+    #print("Standard error of the mean: ", sem)
+    
+    #calculate the confidence interval for confidence level of 95%
+    confidence_level = 0.95
+    df = len(sample) - 1
+    
+    confidence_interval = stats.t.interval(confidence_level, df, loc=sample_mean, scale=sem)
+    #print("Confidence interval: ", confidence_interval)
+    
+    #test whether the population mean is captured by the confidence interval
+    
+    if confidence_interval[0] <= population_mean <= confidence_interval[1]:
+        return 1
+    return 0
+    
+def question1():
+    print("Question 1: \n")
+
+    #generate population of 10,000 random numbers from a normal distribution (mean -> 0 std -> 1)
+    population = np.random.normal(loc=0, scale=1, size=10000)
+    #calculate the population mean
+    population_mean = np.mean(population)
+    
+    print("Population mean: ", population_mean)
+    
+    #repeat the process 100 times to test how it relates to the confidence level required
+    num_captures = 0
+    for _ in range(0,100):
+        num_captures += population_mean_test(population, population_mean)
+    
+    print("The confidence interval captures the population mean ", num_captures, "/100 times.")
+    
+    #How does your result relate to the confidence level you asked for? (refer to txt file)
+
+question1()
